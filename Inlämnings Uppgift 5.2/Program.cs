8f29482a47bd0c89b28a39aa32e9eef05f731b0e
baronvonpurr <robinhood.svensson@hotmail.com>
2023-11-27 21:46:19 +0100

    Add project files.

---
+++ Inlämnings Uppgift 5.2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Uppgift5_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nummer = new int[3];
+
+            nummer[0] = 3;
+            nummer[1] = 1;
+
+            Console.Write("Skriv in ett nummer: ");
+            nummer[2] = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < 3; i++)
+            {
+                Console.Write($"{nummer[i]} ");
+            }
+            Console.ReadKey();
+        }
+    }
+}
